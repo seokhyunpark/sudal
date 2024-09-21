d689d9c794add299cb46d83889e384e869bbd2f8
jieun park <sgu101620@naver.com>
2024-09-21 21:11:12 +0900

    html -> flask 이미지 전송 추가 : app.py sudal.html 수정

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 from gemini import generate_response
+import sys
+
+sys.stdout.reconfigure(encoding='utf-8')
+
 
 
 app = Flask(__name__)
@@ -10,11 +14,20 @@ def index():
 
 @app.route('/generate-ad', methods=['POST'])
 def generate_ad():
-    data = request.json
+    data = request.form
     ratio = data.get('ratio')
     style = data.get('style')
     subject = data.get('subject')
     requirement = data.get('requirement')
+    image=request.files.get('image')
+
+    if image and image.filename != '':
+        filename=image.filename
+        #image.save(f"./uploads/")#uploads폴더 엇으면 오류
+        print(f"이미지 파일 전송 성공")
+    else:
+        print("이미지 파일이 전송되지 않았습니다.")
+
     print(f"비율: {ratio},  스타일 {style}, 주제: {subject}, 요구 사항: {requirement}")
 
     gemini_cmd = "아래 내용을 참고해서 홍보 문구를 30자 내외로 작성해주세요."
