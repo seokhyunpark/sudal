37c5cb52454c96660ba2ffb7acc5faceb3589247
skhnpark <skhnpark125@gmail.com>
2024-09-21 18:00:48 +0900

    Features: 스타일, 주제, 요구사항 입력 받아서 gemini ai로 홍보용 문구 생성 후 팝업

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, jsonify, render_template
 from gemini import generate_response
+
 
 app = Flask(__name__)
 
@@ -8,6 +8,23 @@ app = Flask(__name__)
 def index():
     return render_template('sudal.html')
 
+@app.route('/generate-ad', methods=['POST'])
+def generate_ad():
+    data = request.json
+    ratio = data.get('ratio')
+    style = data.get('style')
+    subject = data.get('subject')
+    requirement = data.get('requirement')
+    print(f"비율: {ratio},  스타일 {style}, 주제: {subject}, 요구 사항: {requirement}")
+
+    gemini_cmd = "아래 내용을 참고해서 홍보 문구를 30자 내외로 작성해주세요."
+    gemini_fmt = "설명은 하지말고 작성한 홍보 문구만 제출해주세요."
+
+    message = gemini_cmd + f"비율: {ratio},  스타일 {style}, 주제: {subject}, 요구 사항: {requirement}" + gemini_fmt
+    response = {'message': generate_response(message)}
+    print(response)
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
